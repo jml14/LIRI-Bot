0d7a3a074cac2a07b0e328177a2f5e1b1d286f88
Jeremy Lipsitz <jeremylipsitz@gmail.com>
2018-01-06 18:18:50 -0600

    Added the keys file

---
+++ keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+exports.twitterKeys = {
+  consumer_key: 'S3gM7oNVRSYrhmhOUXINUbTmH',
+  consumer_secret: 'pPbm93Uf1Co2gBV8gpo9E3WHuHSCmuMnL4TCIx7jTZ65IGW9bM',
+  access_token_key: '949754137431846912-A7JE1kt351P4LcFvaYwsXAVPYQXKh8J',
+  access_token_secret: 'WcxUDztMrZ4myuLj2lMdgZqLizV0VRMPadHvZvy3ZtbDJ',
+}
+exports.spotifyKeys = {
+	id: '494468418c40405491868b9bf9c14d4d',
+	secret: '1f86d4f24967451c9cb87f0fddbd9fa0'
+}
+exports.OMDBKey = '40e9cece'
